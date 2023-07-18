00f30061e94cf0d88f23594e6b91f282baf6b4a0
manovenie <manilovx@gmail.com>
2023-07-18 18:59:31 +0300

    prepare tests for main functions for the app

---
+++ matrix_handler/snail_matrix.py
@@ -0,0 +1,77 @@
+import aiohttp
+import asyncio
+
+
+async def get_text(url: str) -> str:
+    pass
+
+
+def is_quadratic(matrix_list):
+    first_row_len = len(matrix_list[0])
+    row_count = 0
+    for row in matrix_list:
+        if len(row) != first_row_len:
+            return False
+        row_count += 1
+    if row_count != first_row_len:
+        return False
+    return True
+
+
+def prepare_matrix(text: str) -> list[list[int]]:
+    matrix_list = []
+    row = []
+    str_num = ''
+    for char in text:
+        if 48 <= ord(char) <= 57:
+            str_num = str_num + char
+        elif len(str_num) and char.isspace() and char != '\n':
+            row.append(int(str_num))
+            str_num = ''
+        elif len(row) and char == '\n':
+            matrix_list.append(row)
+            row = []
+    # check if matrix is not empty and it's quadratic
+    if matrix_list and is_quadratic(matrix_list):
+        return matrix_list
+    return []
+
+
+def traverse_matrix(quad_matrix: list[list[int]]) -> list[int]:
+    result = []
+    x = 0
+    y = -1     # -1 for lookup check in the 1st if in a while loop
+    x_dir = 0  # -1 0 1
+    y_dir = 1  # -1 0 1 (first move direction : downside)
+    size = len(quad_matrix[0])
+    max_moves_count = size ** 2
+    move_counter = 0
+    while move_counter < max_moves_count:
+        # check not to go beyond x,y boundaries and not to visited cells
+        if (0 <= x + x_dir < size) and (0 <= y + y_dir < size) and (quad_matrix[y+y_dir][x+x_dir] is not None):
+            x += x_dir
+            y += y_dir
+            result.append(quad_matrix[y][x])
+            quad_matrix[y][x] = None
+            move_counter += 1
+        # from downside to right
+        elif y_dir == 1:
+            y_dir = 0
+            x_dir = 1
+        # from right to upside
+        elif x_dir == 1:
+            x_dir = 0
+            y_dir = -1
+        # from upside to left
+        elif y_dir == -1:
+            y_dir = 0
+            x_dir = -1
+        # from left to downside
+        elif x_dir == -1:
+            x_dir = 0
+            y_dir = 1
+    return result
+
+
+async def get_matrix(url: str) -> list[int]:
+    pass
---
+++ tests/test_snail_matrix.py
@@ -0,0 +1,37 @@
+import pytest
+from matrix_handler.snail_matrix import prepare_matrix, traverse_matrix, get_matrix
+
+SOURCE_URL = 'https://raw.githubusercontent.com/avito-tech/python-trainee-assignment/main/matrix.txt'
+TRAVERSAL = [
+    10, 50, 90, 130,
+    140, 150, 160, 120,
+    80, 40, 30, 20,
+    60, 100, 110, 70,
+]
+
+PREPARED_MATRIX = [
+    [10, 20, 30, 40],
+    [50, 60, 70, 80],
+    [90, 100, 110, 120],
+    [130, 140, 150, 160],
+]
+
+
+def test_prepare_matrix():
+    with open("fixtures/test_matrix_string.txt") as test_matrix:
+        assert prepare_matrix(test_matrix.read()) == PREPARED_MATRIX
+
+    # test for empty string instead of matrix
+    assert prepare_matrix("") == []
+
+    # test for non-quadratic matrix from file
+    with open("fixtures/test_non_quad_matrix.txt") as test_non_quad_matrix:
+        assert prepare_matrix(test_non_quad_matrix.read()) == []
+
+
+def test_traverse_matrix():
+    assert traverse_matrix(PREPARED_MATRIX) == TRAVERSAL
+
+
+# def test_get_matrix():
+#     assert asyncio.run(get_matrix(SOURCE_URL)) == TRAVERSAL
